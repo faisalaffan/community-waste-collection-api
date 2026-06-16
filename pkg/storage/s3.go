61e2085667e0e31f536ea85861674ff7b719a38e
Muhammad Faisal Affan <faisallionel@gmail.com>
2026-06-17 02:37:55 +0700

    feat: add S3/MinIO storage client

---
+++ pkg/storage/s3.go
@@ -0,0 +1,48 @@
+package storage
+
+import (
+	"context"
+	"io"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type S3Client struct {
+	client     *minio.Client
+	bucketName string
+}
+
+func NewS3(endpoint, accessKey, secretKey, bucket string, useSSL bool) (*S3Client, error) {
+	client, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	exists, err := client.BucketExists(ctx, bucket)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		err = client.MakeBucket(ctx, bucket, minio.MakeBucketOptions{})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &S3Client{client: client, bucketName: bucket}, nil
+}
+
+func (s *S3Client) Upload(r io.Reader, objectName string, size int64, contentType string) (string, error) {
+	ctx := context.Background()
+	opts := minio.PutObjectOptions{ContentType: contentType}
+	_, err := s.client.PutObject(ctx, s.bucketName, objectName, r, size, opts)
+	if err != nil {
+		return "", err
+	}
+	return objectName, nil
+}
